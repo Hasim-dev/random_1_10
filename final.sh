4d64e916fe35000e8c19ae1bcaaa275f97eab75b
Haşim Mumcu <55324186+Hasim-dev@users.noreply.github.com>
2022-10-30 19:18:33 +0300

    Add files via upload
    Adding final

---
+++ final.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+declare -a LIST=()
+i=0
+while true
+do
+        NUMBER=$(( RANDOM %10 + 1 ))
+        AL=${#LIST[@]}
+        c=0
+        for (( f=0; f<=${AL}; f++ ));
+        do
+                if [[ ${LIST[$f]} -eq $NUMBER ]]
+                then
+                        ((c++))
+                fi
+        done
+        if [[ $c -ne 0 ]];
+        then
+                ((i--))
+        else
+                LIST[$i]=$NUMBER
+        fi
+        ((i++))
+        [[ $i -eq 10 ]] && break
+done
+for p in {0..9}
+do
+#        echo ${LIST[$p]}
+	echo "index: $p value: ${LIST[$p]}"
+done
